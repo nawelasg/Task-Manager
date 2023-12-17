659029080de4aa6d0dba6d7e74e32ae79124b5b1
Nawel Asgar <nawelasg@gmail.com>
2023-12-17 17:46:22 +0530

    Set up Basic routes

---
+++ controllers/tasks.js
@@ -0,0 +1,7 @@
+const getAllTasks = (req,res) =>{
+    res.send(`Get All Items from the Files`)
+}
+
+module.exports = {
+    getAllTasks
+}
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.route('/').get((req,res )=> {
-    res.send('All Items');
-});
+const { getAllTasks,createTask,getTask,updateTask,deleteTask } = require("../controllers/tasks");
+
+router.route('/').get(getAllTasks);
 
 module.exports = router;
 
