1485334613a8f689d662242f663f5bbe0af1a29a
Nawel Asgar <nawelasg@gmail.com>
2023-12-17 17:31:33 +0530

    Created basic routes and Task.js

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
 
+//Setting Up middleware
+app.use(express.json())
+
+
+//Setting up routes
+app.get(`/hello`, (req,res)=>{
+    res.send(`Task Manager App`);   
+})
+
+app.use('/api/v1/tasks', tasks)
+
+
+//ports
 const port = 6969;
 
-app.listen(port, console.log('Server Started at port 6969'));
+app.listen(port, console.log(`Server is Running on Port ${port}`));
