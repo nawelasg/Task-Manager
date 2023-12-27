512ddbeb71050b7cf59e819caa68d327887f5dab
Nawel Asgar <nawelasg@gmail.com>
2023-12-27 17:53:20 +0530

    Setup PORTS and fixed Error-Handler

--- errors/custom-error.js
+++ errors/custom-error.js
@@ -1,12 +1,12 @@
-class CustomeAPIError extends Error {
-    constuctor{ message,statusCode}{
+class CustomAPIError extends Error {
+    constructor(message, statusCode) {
         super(message)
         this.statusCode = statusCode
     }
 }
 
-const createCustomError = (msg,statusCode) =>{
-    return new CustomeAPIError(msg,statusCode)
+const createCustomError = (msg, statusCode) => {
+    return new CustomAPIError(msg, statusCode)
 }
 
 module.exports = { CustomAPIError, createCustomError }
